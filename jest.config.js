ef10df1d3ad3060647c5f41b9bad9831b0cb8a5b
overlookmotel <theoverlookmotel@gmail.com>
2020-04-18 12:01:36 +0100

    Code coverage npm scripts
    `cross-env` handles environment vars on Windows.
    `coveralls` displays code coverage report on web (coveralls.io).

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,7 @@ module.exports = {
     testRegex: "test/.*\\.(js|jsx)$",
     transform: {
         "^.+\\.[t|j]sx?$": "babel-jest"
-    }
+    },
+    coverageDirectory: "coverage",
+    collectCoverageFrom: ["src/**/*.js"]
 };
