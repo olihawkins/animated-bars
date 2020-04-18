36a7af52516b870fdab2d0cf224e6e982f31ba59
overlookmotel <theoverlookmotel@gmail.com>
2020-04-18 14:48:16 +0100

    Configure build with `BUILD_ENV`

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,19 @@
+/* global process */
+
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
 
+// Get build formats
+// Use `BUILD_ENV` to build only specific formats.
+// e.g. `BUILD_ENV=cjs npm run build` or `BUILD_ENV=cjs,esm npm run build`
+// Plain `npm run build` builds them all.
+const formats = getFormats(["cjs", "esm", "umd"]);
+
+// Create build configs
 const configs = [];
-for (const format of ["cjs", "esm", "umd"]) {
+for (const format of formats) {
     configs.push(
         createConfig(format, false),
         createConfig(format, true)
@@ -39,4 +48,23 @@ function createConfig(format, isProduction) {
 function disableCircularDependencyWarnings(warning, warn) {
     if (warning.code === "CIRCULAR_DEPENDENCY") return;
     warn(warning);
+}
+
+function getFormats(allFormats) {
+    const formatsStr = process.env.BUILD_ENV;
+
+    // Default = all formats
+    if (!formatsStr) return allFormats;
+
+    // Parse list of formats
+    // eslint-disable-next-line no-shadow
+    const formats = formatsStr.split(",").map(format => format.toLowerCase());
+    const invalidFormat = formats.find(format => format !== "all" && !allFormats.includes(format));
+    if (invalidFormat != null) {
+        throw new Error(`Unrecognised BUILD_ENV format '${invalidFormat}' - supported formats are ${allFormats.map(format => `'${format}'`).join(", ")} or 'all'`);
+    }
+
+    if (formats.includes("all")) return allFormats;
+
+    return formats;
 }
